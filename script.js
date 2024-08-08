dc6f191be8b8c74bd05a0328b2fe4af830c2d2db
Vad Colcev <vadimkolchev@gmail.com>
2024-08-08 19:39:22 +0300

    Display results in DOM instead of cli

--- script.js
+++ script.js
@@ -23,30 +23,30 @@ function getHumanChoice() {
 
 function playRound(computerChoice, humanChoice) {
     if (computerChoice === humanChoice) {
-        console.log("It's a tie!");
+        setResultText("It's a tie!");
         return "none";
     } else if (computerChoice === "rock") {
         if (humanChoice === "paper") {
-            console.log("You win! Paper beats rock.");
+            setResultText("You win! Paper beats rock.");
             return "human";
         } else if (humanChoice === "scissors") {
-            console.log("You lose! Rock beats scissors.");
+            setResultText("You lose! Rock beats scissors.");
             return "cpu";
         }
     } else if (computerChoice === "paper") {
         if (humanChoice === "scissors") {
-            console.log("You win! Scissors beats paper.");
+            setResultText("You win! Scissors beats paper.");
             return "human";
         } else if (humanChoice === "rock") {
-            console.log("You lose! Paper beats rock.");
+            setResultText("You lose! Paper beats rock.");
             return "cpu";
         }
     } else if (computerChoice === "scissors") {
         if (humanChoice === "rock") {
-            console.log("You win! Rock beats scissors.");
+            setResultText("You win! Rock beats scissors.");
             return "human";
         } else if (humanChoice === "paper") {
-            console.log("You lose! Scissors beat paper.");
+            setResultText("You lose! Scissors beat paper.");
             return "cpu";
         }
     }
@@ -56,19 +56,33 @@ function updateScore(player) {
     scores[player]++;
 }
 
+function displayScore() {
+    humanScore.textContent = scores.humanScore;
+    computerScore.textContent = scores.computerScore;
+}
+
+function setResultText(text) {
+    resultTextArea.textContent = text;
+}
+
 const scores = {
     humanScore: 0,
     computerScore: 0,
 }
 
 const buttonContainer = document.querySelector(".buttons");
+const resultTextArea = document.querySelector(".result-text");
+const humanScore = document.querySelector(".human-score");
+const computerScore = document.querySelector(".computer-score");
+
 buttonContainer.addEventListener('click', (e) => {
     let playerMove = e.target.textContent.toLowerCase();
     let cpuMove = getComputerChoice();
     let winner = playRound(cpuMove, playerMove);
-        if (winner !== "none") {
-            winner === "human" ? updateScore("humanScore") : updateScore("computerScore");
-        }
+    if (winner !== "none") {
+        winner === "human" ? updateScore("humanScore") : updateScore("computerScore");
+    }
+    displayScore();
 })
 
 //playGame();
